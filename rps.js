0d6f8cf9046bd955628e48952467aeb17a756178
Yamini Rathish <yamini.rathish@gmail.com>
2023-07-03 11:19:30 +0200

    simple game using js

---
+++ rps.js
@@ -0,0 +1,40 @@
+//const prompt=require("prompt-sync")({sigint:true});
+
+function Play() {
+  let question = prompt("Shall we play Rock,Paper,Scissors ?");
+  //console.log(question);
+  if (question === null || question === undefined) {
+    console.log("You have to enter something");
+    return; // Stop further execution of the function
+  }
+  if (question.length) {
+    //console.log(question);
+    let player = question.toLowerCase();
+    console.log("player :" + player);
+    if (player === "rock" || player === "paper" || player === "scissors") {
+      let arr = ["rock", "paper", "scissors"];
+      let opponent = arr[Math.floor(Math.random() * (arr.length - 1))];
+      console.log(" opponent :" + opponent);
+      if (player === opponent) {
+        alert("the game is a tie");
+      } else if (
+        (player === "rock" && opponent === "paper") ||
+        (player === "paper" && opponent === "scissors") ||
+        (player === "scissors" && opponent === "rock")
+      ) {
+        alert("Opponent won");
+      } else if (
+        (player === "rock" && opponent === "scissors") ||
+        (player === "paper" && opponent === "rock") ||
+        (player === "scissors" && opponent === "paper")
+      ) {
+        alert("You won");
+      }
+    } else {
+      console.log("enter anything from rock,paper or scissors");
+    }
+  } else {
+    console.log("you didn't enter anything");
+  }
+}
+Play();
